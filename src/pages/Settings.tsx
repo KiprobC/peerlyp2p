b5cd3e77ff99798e19b185156e85c5d3f591bcd5
KiprobC <kiprobcaleb@gmail.com>
2026-07-21 18:31:39 +0300

    make security dialog scrollable on smaller screens

--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -470,7 +470,7 @@ const Settings = () => {
 
       {/* Security Dialog */}
       <Dialog open={showSecurityDialog} onOpenChange={setShowSecurityDialog}>
-        <DialogContent className="max-w-md">
+        <DialogContent className="max-w-md max-h-[90vh] flex flex-col">
           <DialogHeader>
             <DialogTitle className="flex items-center gap-2">
               <Shield className="w-5 h-5 text-primary" />
